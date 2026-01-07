5f6d1474c357c71ca881aa538e7b581e0e4e3bf8
Fathima Rafah <fathimarafah03@gmail.com>
2026-01-07 10:09:24 +0530

    correct the error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://roam-together.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -43,7 +43,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://roam-together.netlify.app/"],
     methods: ["GET", "POST"],
     credentials: true,
   },
